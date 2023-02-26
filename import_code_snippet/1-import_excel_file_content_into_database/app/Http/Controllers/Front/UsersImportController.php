d2882263f8b62fdd771dee649bfd0f0fc9cb12c8
Keyur Rajpara <keyurrajpara1@gmail.com>
2023-02-26 20:02:03 +0530

    Import snippet 1

---
+++ import_code_snippet/1-import_excel_file_content_into_database/app/Http/Controllers/Front/UsersImportController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\UsersImport;
+
+class UsersImportController extends Controller
+{
+    public function show(Request $request){
+        return view('front.users.import');
+    }
+    public function store(Request $request){
+        $file = $request->file('file');
+        Excel::import(new UsersImport, $file);
+        return back()->withStatus('Excel file imported successfully');
+    }
+}
